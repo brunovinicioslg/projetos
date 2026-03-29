73a7b7e85cd174eafcefe6cf731cdfffdbdbbcc4
Bruno Vinicius <104902660+brunovinicioslg@users.noreply.github.com>
2026-03-29 10:10:35 -0300

    feat: adiciona src/Models/CodigoModel.cs — Sistema de Sorteio

---
+++ SORTEIO/src/Models/CodigoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SORTEIO.Models
+{
+    public class CodigoModel
+    {
+        public int Id { get; set; }
+        public int Qr {  get; set; }
+        public string? Codigo { get; set;}
+
+        public DateTime DataCadastro { get; set; }
+
+    }
+}
