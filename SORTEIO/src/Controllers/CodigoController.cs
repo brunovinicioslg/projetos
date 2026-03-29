c46896fc8bac97a57b1d3f034e66589ffb5f5874
Bruno Vinicius <104902660+brunovinicioslg@users.noreply.github.com>
2026-03-29 10:10:36 -0300

    feat: adiciona src/Controllers/CodigoController.cs — Sistema de Sorteio

---
+++ SORTEIO/src/Controllers/CodigoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SORTEIO.Models;
+using SORTEIO.Repositorio;
+
+namespace SORTEIO.Controllers
+{
+    public class CodigoController : Controller
+    {
+        private readonly ICodigoRepositorio _codigoRepositorio;
+        public CodigoController(ICodigoRepositorio codigoRepositorio)
+        {
+            _codigoRepositorio = codigoRepositorio;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Criar(CodigoModel cod) {
+
+            _codigoRepositorio.Adicionar(cod);
+            return RedirectToAction("Index");
+           
+        }
+    }
+}
