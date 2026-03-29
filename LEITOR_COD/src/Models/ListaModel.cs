0eb1195ade51651d797e0cf0b872015b45758c22
Bruno Vinicius <104902660+brunovinicioslg@users.noreply.github.com>
2026-03-29 10:10:40 -0300

    feat: adiciona src/Models/ListaModel.cs — Leitor de Código de Barras

---
+++ LEITOR_COD/src/Models/ListaModel.cs
@@ -0,0 +1,15 @@
+﻿namespace LEITOR.Models
+{
+    public class ListaModel
+    {
+        public int Id { get; set; }
+        public string? Descricao { get; set; }
+        public int? Quantidade { get; set; }
+        public int Status {  get; set; }
+
+
+        public int? UsuarioId { get; set; }
+        public UsuarioModel Usuario { get; set; }
+
+    }
+}
